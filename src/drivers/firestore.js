fec817d5d90e71afb6e4dd3a09291a092cdd9675
HugoTalledos <hugo.talledos@bpsmart.ai>
2021-03-26 18:52:25 -0500

    Fix access credentials

--- src/drivers/firestore.js
+++ src/drivers/firestore.js
@@ -1,10 +1,29 @@
 const admin = require('firebase-admin');
 
+const env = process.env.ENV;
+
+let serviceAccount;
+const remoteEnvs = ['dev'];
+
+try {
+  serviceAccount = remoteEnvs.includes(env)
+    ? null
+    : require('../../codec-idired-config.json')
+} catch (e) {
+  console.log(e);
+}
+
 let app;
 let firestoreRef;
+
+
 try {
   app = admin.initializeApp({
-    credential: admin.credential.applicationDefault()
+    credential: remoteEnvs.includes(env)
+      ? admin.credential.applicationDefault()
+      : serviceAccount
+        ? admin.credential.cert(serviceAccount)
+        : null,
   });
   firestoreRef = admin.firestore();
 } catch (e) {
